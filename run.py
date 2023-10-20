156f36fc63f21c55748831bbff17f7780656b435
oscargdzs <ogomezd@fp.insjoaquimmir.cat>
2023-10-20 15:56:43 +0200

    sqlarchemy

--- run.py
+++ run.py
@@ -1,97 +1,168 @@
-from flask import Flask, render_template , g, request, url_for, abort, redirect
-import sqlite3, datetime
+from flask import Flask ,render_template ,g ,request, redirect, url_for
+import sqlite3, datetime, os
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './upload'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 DATABASE = 'database.db'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# ruta absoluta d'aquesta carpeta
+basedir = os.path.abspath(os.path.dirname(__file__)) 
+
+# paràmetre que farà servir SQLAlchemy per a connectar-se
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + basedir + "/database.db"
+# mostre als logs les ordres SQL que s'executen
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy()
+db.init_app(app)
+now = datetime.datetime.utcnow
+
+class categoria(db.Model):
+    __tablename__ = "categories"
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    slug = db.Column(db.Text, nullable=False)
+
+class product(db.Model):
+    __tablename__ = "products"
+    id              = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title           = db.Column(db.Text, nullable=False)
+    description     = db.Column(db.Text, nullable=False)
+    photo           = db.Column(db.Text, nullable=False)
+    price           = db.Column(db.Numeric(10,2), nullable=False)
+    category_id     = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=False)
+    seller_id       = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    created         = db.Column(db.DateTime, default=now)
+    updated         = db.Column(db.DateTime, default=now)
+
+
+# taula users
+
+class user(db.Model):
+    __tablename__   = "users"
+    id              = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name            = db.Column(db.Text, nullable=False, unique=True)
+    email           = db.Column(db.Text, nullable=False, unique=True)
+    password        = db.Column(db.Text, nullable=False)
+    created         = db.Column(db.DateTime, default=now)
+    update          = db.Column(db.DateTime, default=now)
+
+# class Order(db.Model):
+#     __tablename__ = "orders"
+#     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     product_id = db.Column(db.Integer, nullable=False)
+#     buyer_id = db.Column(db.Integer, nullable=False)
+#     created = db.Column(db.DateTime, default=now)
+#     UniqueConstraint("product_id", "buyer_id", name="uc_product_buyer")
+#     category_id = db.Column(db.ForeignKey("categories.id"), nullable=False)
+#     seller_id = db.Column(db.ForeignKey("users.id"), nullable=False)
+
+# class ConfirmedOrder(db.Model):
+#     __tablename__ = "confirmed_orders"
+#     order_id = db.Column(db.Integer, primary_key=True)
+#     created = db.Column(db.DateTime, default=now)
+#     order_id = db.Column(db.ForeignKey("orders.id"), nullable=False)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def get_db():
+    # sqlite3_database_path = DATABASE
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route("/")
 def hello_world():
-    return render_template("hello.html")
-
-def get_db():
-    con = sqlite3.connect(DATABASE)
-    con.row_factory = sqlite3.Row
-    return con
+    return render_template('hello.html')
 
 @app.route("/products/list")
-def item_list():
-      with get_db() as con:
-        res = con.execute("SELECT * FROM products ORDER BY id ASC")
-        items = res.fetchall()
-      
-      return render_template("products/list.html",items = items)
-
-
+def list():
+    items = db.session.query(product).all()
+    return render_template("products/list.html",items = items)
 
 @app.route("/products/create", methods = ['GET','POST'])
 def create():
     if request.method == 'POST':
-        datos       = request.form
-        title       = datos.get('nom')
-        description = datos.get('descripcio')
-        price       = int(datos.get('preu'))
-        foto        = 'hola'
-        created     = datetime.datetime.now()
-        updated     = datetime.datetime.now()
+        datos           = request.form
+        title           = datos.get('title')
+        description     = datos.get('description')
+        price           = int(datos.get('price'))
+        fotoBD          = request.files['photo'].filename
+        archivo         = request.files['photo']
+        created         = datetime.datetime.now()
+        updated         = datetime.datetime.now()         
+        filename        = secure_filename(archivo.filename)
+        archivo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         
-        with get_db() as conn:
-            sql = "INSERT INTO products (title, description, photo, price, created, updated) VALUES (?, ?, ?, ?, ?, ?)"
-            # app.logger.info('SQL: %s', sql)
-            conn.execute(sql, (title, description, foto, price, created, updated))
+
+        nou_item = product()
+        nou_item.title  = title
+        nou_item.description  = description
+        nou_item.price = price
+        nou_item.photo  = fotoBD
+        nou_item.created  = created
+        nou_item.updated = updated
+
+        db.session.add(nou_item)
+        db.session.commit()
+
         return redirect(url_for('list'))
     else:
         return render_template('products/create.html')
-
     
 @app.route("/products/read/<int:id>")
 def read(id):
-    app.logger.debug("INT" if (type(id) is int) else "OTHER")
-    with get_db() as conn:
-        sql = "SELECT * FROM products WHERE id = " + str(id)
-        resultat = conn.execute(sql)
-        items = resultat.fetchall()
+    # app.logger.debug("INT" if (type(id) is int) else "OTHER")
+    items = db.session.query(product).where(product.id == id)
     return render_template("products/list.html",items = items)
 
-@app.route("/products/delete/<int:product_id>", methods=['GET', 'POST'])
-def delete(product_id):
-    with get_db() as conn:
-        if request.method == 'POST':
-            sql = "DELETE FROM products WHERE id = ?"
-            conn.execute(sql, (product_id,))
-            return redirect(url_for('item_list'))
-        else:
-            sql = "SELECT * FROM products WHERE id = ?"
-            res = conn.execute(sql, (product_id,))
-            product = res.fetchone()
-            if product:
-                return render_template('products/delete.html', product=product)
-            else:
-                return "Producto no encontrado", 404
-            
-@app.route("/products/update/<int:product_id>", methods=['GET', 'POST'])
-def update(product_id):
-    with get_db() as conn:
-        if request.method == 'POST':
-            datos       = request.form
-            title       = datos.get('nom')
-            description = datos.get('descripcio')
-            price       = int(datos.get('preu'))
-            foto        = 'hola' # O modifica esto según la lógica de tu aplicación
-            updated     = datetime.datetime.now()
 
-            sql = """
-            UPDATE products 
-            SET title = ?, description = ?, photo = ?, price = ?, updated = ? 
-            WHERE id = ?
-            """
-            conn.execute(sql, (title, description, foto, price, updated, product_id))
-            return redirect(url_for('item_list'))
+@app.route("/products/update/<int:id>", methods = ['GET', 'POST'])
+def update(id):
+    item = db.session.query(product).filter(product.id == id).one()
+    if request.method == 'GET':
+        # with get_db() as conn:
+        #     results = conn.execute("SELECT * FROM products WHERE id = " + str(id))
+        #     items = results.fetchall()
 
-        else:
-            sql = "SELECT * FROM products WHERE id = ?"
-            res = conn.execute(sql, (product_id,))
-            product = res.fetchone()
-            if product:
-                return render_template('products/update.html', product=product)
-            else:
-                return "Producto no encontrado", 404
+        return render_template('products/update.html', item = item)
+    
+    elif request.method == 'POST':
+        # with get_db() as conn:
+        datos           = request.form
+        description     = datos.get('description')
+        title           = datos.get('title')
+        price           = float(datos.get('price'))
+        foto            = request.files['photo'].filename
+        updated         = datetime.datetime.now()
+            # conn.execute("UPDATE products SET title = ?, description = ?, photo = ?, price = ?, updated = ? WHERE id = ?", 
+            #     (title, description, foto, price, updated, id))
+        item.title          = title
+        item.description    = description
+        item.price          = price
+        item.photo          = foto
+        item.updated        = updated
+
+        # update!
+        db.session.add(item)
+        db.session.commit()
+        return redirect(url_for('list'))
+
+@app.route("/products/delete/<int:id>", methods = ['GET', 'POST'])  
+def delete(id):
+    item = db.session.query(product).filter(product.id == id).one()
+
+    db.session.delete(item)
+    db.session.commit()
+
+    return redirect(url_for('list'));
+
